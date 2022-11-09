ab6aa8e4ff87df0a62fab9ca7a31a4efe980dbcf
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-11-09 10:51:21 +0000

    test: kint-components
    Default kint components mocks now include the top level CustomProperties mocks. These can be unmocked in whichever test that is required

--- jest/mocks/mockKintComponents.js
+++ jest/mocks/mockKintComponents.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
 
+/* If we need different mock behaviour in a given test we can use unmock like:
+
+  jest.unmock('@k-int/stripes-kint-components');
+  jest.mock('@k-int/stripes-kint-components', () => ({
+    CustomPropertiesFilter: <Whatever mock implementation we wish>
+  }));
+*/
+
 const mockKintComponents = {
   useIntlKeyStore:() => () => null,
   useRefdata: jest.fn().mockReturnValue([]),
@@ -16,6 +24,10 @@ const mockKintComponents = {
 
     return ({ data: returnShape, isLoading: false });
   }),
+  CustomPropertiesEdit: () => <div>CustomPropertiesEdit</div>,
+  CustomPropertiesFilter: () => <div>CustomPropertiesFilter</div>,
+  CustomPropertyCard: () => <div>CustomPropertyCard</div>,
+  CustomPropertiesView: () => <div>CustomPropertiesView</div>,
 };
 
 export default mockKintComponents;
