88dbd562f8a05a3583782f7e78de441b85462859
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-11-09 16:57:28 +0000

    test: Mock erm-components
    Added some mocks for erm-components

--- jest/mocks/mockErmComponents.js
+++ jest/mocks/mockErmComponents.js
@@ -18,7 +18,24 @@ const mockErmComponents = {
   useHandleSubmitSearch: jest.fn(() => ({
     handleSubmitSearch: jest.fn()
   })),
-  useAsyncValidation: jest.fn(() => '')
+  useAsyncValidation: jest.fn(() => ''),
+  handleSaveKeyCommand: jest.fn(),
+  AlternativeNamesFieldArray: () => <div>AlternativeNamesFieldArray</div>,
+  CustomMetaSection: () => <div>CustomMetaSection</div>,
+  DateFilter: () => <div>DateFilter</div>,
+  DocumentCard: () => <div>DocumentCard</div>,
+  DocumentsFieldArray: () => <div>DocumentsFieldArray</div>,
+  DuplicateModal: () => <div>DuplicateModal</div>,
+  InternalContactCard: () => <div>InternalContactCard</div>,
+  InternalContactSelection: () => <div>InternalContactSelection</div>,
+  InternalContactsFieldArray: () => <div>InternalContactsFieldArray</div>,
+  LicenseCard: () => <div>LicenseCard</div>,
+  LicenseEndDate: () => <div>LicenseEndDate</div>,
+  OrganizationSelection: () => <div>OrganizationSelection</div>,
+  OrganizationsFieldArray: () => <div>OrganizationsFieldArray</div>,
+  SerialCoverage: () => <div>SerialCoverage</div>,
+  ViewOrganizationCard: () => <div>ViewOrganizationCard</div>,
+
 };
 
 export default mockErmComponents;
