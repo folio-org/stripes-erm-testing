79f41d605b5180522761da3e33c4884dd6b82205
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-11-17 13:18:32 +0000

    test: TestForm tweaks
    Added ability to render function as child of TestForm, in order to grab form information. Also passed setFieldData mutator by default, since we use that across ERM to set warnings
    
    refs ERM-2465

--- jest/helpers/TestForm.js
+++ jest/helpers/TestForm.js
@@ -1,25 +1,56 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
 import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
+import setFieldData from 'final-form-set-field-data';
+
 import { Button } from '@folio/stripes/components';
+
+// Allow TestForm to work like a regular Form, and allow children rendering as function or node.
+// Also include setFieldData mutator
 
 const TestForm = ({
   children,
   mutators,
   ...formProps
 }) => {
+  /*
+    This allows us to do things like:
+
+    <TestForm>
+      {({ form }) => (
+        <FormComponent form={form} />
+      )}
+    </TestForm>
+
+    For those instances where we would normally pass those values down from the form up above.
+    Otherwise (such as in those cases where we grab those values from form context), we can render
+    directly as children like we did before:
+
+    <TestForm>
+      <FormComponent />
+    </TestForm>
+   */
+
+  const renderChildren = (props) => {
+    if (typeof children === 'function') {
+      return children({ ...props });
+    }
+
+    return children;
+  };
+
   return (
     <Form
       mutators={{
+        setFieldData,
         ...arrayMutators,
         ...mutators
       }}
       {...formProps}
     >
-      {({ handleSubmit }) => (
+      {({ handleSubmit, ...otherFormProps }) => (
         <form onSubmit={handleSubmit}>
-          {children}
+          {renderChildren(otherFormProps)}
           <Button data-testid="submit" id="submit" type="submit">
             Submit
           </Button>
@@ -28,4 +59,5 @@ const TestForm = ({
     </Form>
   );
 };
+
 export default TestForm;
