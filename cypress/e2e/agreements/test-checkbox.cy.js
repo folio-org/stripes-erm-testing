728c1e6822626f5925ff046de6a0d809dc89d6b9
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-05-08 20:05:44 +0200

    ERM-2773
    * modify test-checkbox temporary test

--- cypress/e2e/agreements/test-checkbox.cy.js
+++ cypress/e2e/agreements/test-checkbox.cy.js
@@ -7,8 +7,9 @@ import {
 } from '@folio/stripes-testing';
 
 const hideKbCheckbox = Checkbox({ id: 'hideEResourcesFunctionality' });// Checkbox('Hide internal agreements knowledgebase');
+const saveButton = Button('Save');
 // const hideKbCheckbox = Checkbox('Hide internal agreements knowledgebase');
-const isChecked = false; // variable to store checkbox state
+let isChecked = false; // variable to store checkbox state
 
 describe('Agreement line with internal resource', () => {
   before(() => {
@@ -18,26 +19,42 @@ describe('Agreement line with internal resource', () => {
     cy.visit('/settings/erm/general');
     cy.expect(hideKbCheckbox.exists());
     // Check if the checkbox is checked, and uncheck it if it is
-    // cy.get(hideKbCheckbox).then(($checkbox) => {
-    //   if ($checkbox.prop('checked')) {
-    //   if ($checkbox.is({ checked: true })) {
-    //     hideKbCheckbox.click();
-    //   }
-    // });
-    // Assert that the checkbox is now unchecked
-    // console.log('checkbox is unchecked test');
-    // cy.expect(hideKbCheckbox.is({ checked: false }));
+    cy.get('input[type="checkbox"][name="hideEResourcesFunctionality"]').then(($checkbox) => {
+      // console.log('$checkbox %o', $checkbox);
+      console.log('$checkbox:', $checkbox);
+      console.log('checked:', $checkbox.prop('checked'));
+      console.log('value:', $checkbox.attr('value'));
+      if ($checkbox.is(':checked')) {
+        // if ($checkbox.prop('checked')) {
+        isChecked = true;
+        console.log('isChecked %o', isChecked);
+        $checkbox.click();
+        console.log('saveButton');
+        cy.expect(saveButton.is({ visible: true }));
+        cy.do(saveButton.click());
+      }
+    });
+
+    // if (isChecked === true) {
+    //   // cy.expect(saveButton.exists());
+    //   console.log('saveButton');
+    //   cy.expect(saveButton.is({ visible: true }));
+    //   cy.do(saveButton.click());
+    // }
   });
+  // Assert that the checkbox is now unchecked
+  // console.log('checkbox is unchecked test');
+  // cy.expect(hideKbCheckbox.is({ checked: false }));
 
   describe('do not hide internal agreements knowledgebase', () => {
     it('should find checkbox checked', () => {
-      cy.visit('/settings/erm/general');
+      // cy.visit('/settings/erm/general');
       cy.expect(hideKbCheckbox.exists());
       cy.expect(hideKbCheckbox.is({ checked: true }));
     });
 
     it('should find checkbox unchecked', () => {
-      cy.visit('/settings/erm/general');
+      // cy.visit('/settings/erm/general');
       cy.expect(hideKbCheckbox.exists());
       cy.expect(hideKbCheckbox.is({ checked: false }));
     });
