0e373ece28ea602cdf7ed6fc839b2a9859f0a264
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-07-10 16:36:48 +0200

    ERM-2812 Create an Agreement and Delete an Agreement
    * add before and after hook to e2e.js to set and unset language

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,3 +19,32 @@ require('cypress-grep')();
 Cypress.on('window:before:load', window => {
   Object.defineProperty(window.navigator, 'language', { value: 'en' });
 });
+
+const defaultTestLanguage = 'en-US';
+before(() => {
+  cy.login(Cypress.env('login_username'), Cypress.env('login_password'));
+  cy.getAdminToken();
+  cy.visit('/settings/tenant-settings/locale');
+  cy.get('select#locale').invoke('val').then((selectedValue) => {
+    Cypress.env('localeValue', selectedValue);
+    if (selectedValue !== defaultTestLanguage) {
+      cy.get('select#locale').select(defaultTestLanguage);
+      cy.get('button[type="submit"]').click();
+    }
+  });
+
+  cy.logout();
+});
+
+after(() => {
+  cy.login(Cypress.env('login_username'), Cypress.env('login_password'));
+  cy.getAdminToken();
+  cy.visit('/settings/tenant-settings/locale');
+  cy.get('select#locale').invoke('val').then((selectedValue) => {
+    if (selectedValue !== Cypress.env('localeValue')) {
+      cy.get('select#locale').select(Cypress.env('localeValue'));
+      cy.get('button[type="submit"]').click();
+    }
+  });
+  cy.logout();
+});
