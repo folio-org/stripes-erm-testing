59b2649c61bd8fa2161102ec8a389105c9eb4f60
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2024-03-26 19:15:17 +0100

    * fix utc format error for date w/o time

--- cypress/support/utils/dateTools.js
+++ cypress/support/utils/dateTools.js
@@ -8,23 +8,26 @@ export default {
     return dayjs.utc().format('MM/DD/YYYY');
   },
 
+  // do not use utc in the following date formatters w/o time
+  // as it will cause the date to be off by one day
+
   getDateAfter: (date) => {
-    return dayjs.utc(date).add(1, 'day').format('MM/DD/YYYY');
+    return dayjs(date).add(1, 'day').format('MM/DD/YYYY');
   },
 
   getDateBefore: (date) => {
-    return dayjs.utc(date).add(-1, 'day').format('MM/DD/YYYY');
+    return dayjs(date).add(-1, 'day').format('MM/DD/YYYY');
   },
 
   getApiDate: (date) => {
-    return dayjs.utc(date).format('YYYY-MM-DD');
+    return dayjs(date).format('YYYY-MM-DD');
   },
 
-  getDateNoZeros(date) {
-    return dayjs.utc(date).format('M/D/YYYY');
+  getDateNoZeros: (date) => {
+    return dayjs(date).format('M/D/YYYY');
   },
 
-  getFormattedDateWithTime(date) {
+  getFormattedDateWithTime: (date) => {
     return dayjs.utc(date).format('M/D/YYYY h:mm A');
   },
 };
