60d24b0096f0bdac7155b57f294b98db45923248
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-04-05 14:42:28 +0100

    test: WIP!! default mock Typedown
    Added default mockTypedown with getterFunction to enable implementors to set up with a given array of options.
    
    Currently breaks jest manual mocks -- not sure why yet

--- jest/mocks/mockKintComponents.js
+++ jest/mocks/mockKintComponents.js
@@ -1,4 +1,10 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
+
+import {
+  Button as MockButton,
+  KeyValue as MockKeyValue
+} from '@folio/stripes/components';
 
 /* If we need different mock behaviour in a given test we can use unmock like:
 
@@ -7,6 +13,34 @@ import { useState } from 'react';
     CustomPropertiesFilter: <Whatever mock implementation we wish>
   }));
 */
+
+const getMockTypedown = (optionArray = []) => ({
+  id,
+  input: { onChange, value },
+  renderFooter,
+  renderListItem
+}) => {
+  return (
+    <div>
+      {`Typedown-${id}`}
+      <MockKeyValue
+        label={`Typedown-${id}-selected-option`}
+        value={value ? renderListItem(value, '') : 'Nothing selected'}
+      />
+      {optionArray.map(seq => {
+        return (
+          <MockButton
+            key={`Typedown-${id}-option-${seq.id}`}
+            onClick={() => onChange(seq)}
+          >
+            {`Typedown-${id}-option-${seq.id}`}
+          </MockButton>
+        );
+      })}
+      {renderFooter()}
+    </div>
+  );
+};
 
 const mockKintComponents = {
   useIntlKeyStore:() => () => null,
@@ -30,6 +64,7 @@ const mockKintComponents = {
   CustomPropertiesView: () => <div>CustomPropertiesView</div>,
   EditableRefdataCategoryList: () => <div>EditableRefdataCategoryList</div>,
   EditableRefdataList: () => <div>EditableRefdataList</div>,
+  Typedown: getMockTypedown()
 };
 
 export default mockKintComponents;
