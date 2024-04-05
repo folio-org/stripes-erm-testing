e219c68a01dfb40d7917be138f1105562588bf91
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-04-05 15:21:45 +0100

    test: mockTypedownGetter
    Moved files around, mockTypedownGetter can't be included in mocks index because they're used for manual mocking, and the circularity of them then requiring stripes-components causes issues. To that end, set up a new directory for these more complicated mocked components which we can then use in a shared manner between modules "componentMocks"

---
+++ jest/componentMocks/mockTypedownGetter.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const {
+  Button: MockButton,
+  KeyValue: MockKeyValue
+} = jest.requireActual('@folio/stripes-components');
+
+const mockTypedownGetter = (optionArray = []) => ({
+  id,
+  input: { onChange, value },
+  renderFooter,
+  renderListItem
+}) => {
+  return (
+    <div>
+      {`Typedown-${id}`}
+      <MockKeyValue
+        label={`Typedown-${id}-selected-option`}
+        value={value ? renderListItem(value, '') : 'Nothing selected'}
+      />
+      {optionArray.map(seq => {
+        return (
+          <MockButton
+            key={`Typedown-${id}-option-${seq.id}`}
+            onClick={() => onChange(seq)}
+          >
+            {`Typedown-${id}-option-${seq.id}`}
+          </MockButton>
+        );
+      })}
+      {renderFooter()}
+    </div>
+  );
+};
+
+export default mockTypedownGetter;
--- jest/mocks/mockKintComponents.js
+++ jest/mocks/mockKintComponents.js
@@ -1,10 +1,4 @@
-/* eslint-disable react/prop-types */
 import { useState } from 'react';
-
-import {
-  Button as MockButton,
-  KeyValue as MockKeyValue
-} from '@folio/stripes/components';
 
 /* If we need different mock behaviour in a given test we can use unmock like:
 
@@ -13,34 +7,6 @@ import {
     CustomPropertiesFilter: <Whatever mock implementation we wish>
   }));
 */
-
-const getMockTypedown = (optionArray = []) => ({
-  id,
-  input: { onChange, value },
-  renderFooter,
-  renderListItem
-}) => {
-  return (
-    <div>
-      {`Typedown-${id}`}
-      <MockKeyValue
-        label={`Typedown-${id}-selected-option`}
-        value={value ? renderListItem(value, '') : 'Nothing selected'}
-      />
-      {optionArray.map(seq => {
-        return (
-          <MockButton
-            key={`Typedown-${id}-option-${seq.id}`}
-            onClick={() => onChange(seq)}
-          >
-            {`Typedown-${id}-option-${seq.id}`}
-          </MockButton>
-        );
-      })}
-      {renderFooter()}
-    </div>
-  );
-};
 
 const mockKintComponents = {
   useIntlKeyStore:() => () => null,
@@ -63,8 +29,7 @@ const mockKintComponents = {
   CustomPropertyCard: () => <div>CustomPropertyCard</div>,
   CustomPropertiesView: () => <div>CustomPropertiesView</div>,
   EditableRefdataCategoryList: () => <div>EditableRefdataCategoryList</div>,
-  EditableRefdataList: () => <div>EditableRefdataList</div>,
-  Typedown: getMockTypedown()
+  EditableRefdataList: () => <div>EditableRefdataList</div>
 };
 
 export default mockKintComponents;
