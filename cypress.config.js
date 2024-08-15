ea2c46ea23935e155fee848fb1790d10046d3b44
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2024-08-15 18:07:47 +0200

    debug

--- cypress.config.js
+++ cypress.config.js
@@ -17,12 +17,6 @@ module.exports = defineConfig({
     grepOmitFiltered: true,
   },
   e2e: {
-    configurations: {
-      ci: {
-        record: true,
-        parallel: true,
-      }
-    },
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
