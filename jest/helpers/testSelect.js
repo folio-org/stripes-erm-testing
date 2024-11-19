ccfc470610bdeaf38323c3674055475cb20ae9ed
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-11-19 15:42:45 +0000

    test: Added testSelect helper function

---
+++ jest/helpers/testSelect.js
@@ -0,0 +1,26 @@
+import { waitFor } from '@folio/jest-config-stripes/testing-library/react';
+import { Select } from '@folio/stripes-erm-testing';
+
+const testSelect = ({
+  selector,
+  selectorName,
+  optionsArray
+}) => {
+  const actualName = selectorName ?? JSON.stringify(selector);
+  return describe.each(optionsArray)(`choosing ${actualName} option %s`, (selectOption) => {
+    beforeEach(async () => {
+      await waitFor(async () => {
+        await Select(selector).choose(selectOption);
+      });
+    });
+
+    test(`Option ${selectOption} is selected`, async () => {
+      await waitFor(async () => {
+        await Select(selector).has({ checkedOptionText: selectOption });
+      });
+    });
+  });
+};
+
+export default testSelect;
+
