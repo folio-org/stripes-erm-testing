7a3e3db019f24b0210afc2753a0dfd96ce296f46
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-03-13 14:54:47 +0000

    test: Suppress certain errors
    findDOMNode errors specifically cause us a lot of issues and come exclusively (thus far) from outside our module.
    
    This is also true of props of type `on...` passed directly to html nodes. In general these are happening in Stripes not in our modules.
    
    To that end, we wish to lower noise and allow ourselves to see actual errors and warnings in tests, and so we intercept console error itself and only display those errors not matching our list
    
    This change lowers the number of errors in ui-agreements from 33 down to 4

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,1 +1,13 @@
 import './directMocks/window.mock';
+
+const suppressErrors = [
+  'findDOMNode', // This should be REMOVED once findDOMNode is properly stamped out from all dependencies
+  'Unknown event handler property `%s`' // These tend to come from stripes having "on..." props passed directly to html nodes. Might need to be reinstated
+];
+
+const originalError = console.error;
+console.error = jest.spyOn(console, 'error').mockImplementation((message, ...rest) => {
+  if (suppressErrors.every(m => !message.includes(m))) {
+    originalError.apply(console, [message, ...rest]);
+  }
+});
