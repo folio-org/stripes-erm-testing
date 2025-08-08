e5466b35bc739338069dada5b9d8086018cdfeab
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-08-08 15:08:58 +0100

    feat: Icon interactor
    Icons should now not instantly crash when in tests. This allows for testing of icons themselves.
    
    Icon and IconElement interactors in place to help with this.
    However, that same fix ends up causing a console proptypes error, in jest harness we want to very specifically quash that error but no others.

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,13 +1,23 @@
 import './directMocks/window.mock';
 
 const suppressErrors = [
-  'findDOMNode', // This should be REMOVED once findDOMNode is properly stamped out from all dependencies
-  'Unknown event handler property `%s`' // These tend to come from stripes having "on..." props passed directly to html nodes. Might need to be reinstated
+  { errorString: 'findDOMNode' }, // This should be REMOVED once findDOMNode is properly stamped out from all dependencies
+  { errorString: 'Unknown event handler property `%s`' }, // These tend to come from stripes having "on..." props passed directly to html nodes. Might need to be reinstated
+  { errorString: 'Failed %s type: %s%s', context: 'Invalid prop `icon` supplied to `Icon`, expected one of type [function].' }, // Specifically this proptype fails in jest as it usually uses clever webpack stuff
 ];
 
 const originalError = console.error;
 console.error = jest.spyOn(console, 'error').mockImplementation((message, ...rest) => {
-  if (suppressErrors.every(m => message.includes instanceof Function && !message.includes(m))) {
+  const contextArray = [...rest];
+  if (suppressErrors.every(m => {
+    return (
+      message.includes instanceof Function &&
+      (
+        (!m.context && !message.includes(m.errorString)) || // If there is no context, and errorString matches, suppress
+        (message.includes(m.errorString) && !contextArray.includes(m.context)) // If the errorString matches AND context includes, suppress
+      )
+    );
+  })) {
     originalError.apply(console, [message, ...rest]);
   }
 });
